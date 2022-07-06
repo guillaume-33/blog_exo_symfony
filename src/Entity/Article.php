7b66c5ce7ca560709ced9233e96cd4857e24592e
guillaume <laloum.guillaume@gmail.com>
2022-07-06 10:30:41 +0200

    exo BDD

---
+++ src/Entity/Article.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ */
+class Article
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    public $id;
+
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    public $title;
+}
