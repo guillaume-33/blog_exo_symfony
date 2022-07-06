ce856c15dcffbd4a3a93f71f24294b1aa0800cb0
guillaume <laloum.guillaume@gmail.com>
2022-07-06 11:35:22 +0200

    exo BDD +comm

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -21,4 +21,30 @@ class Article // le nom de notre table(entité) TOUJOURS AVEC lA PREMIERE LETTRE
      * @ORM\Column(type="string") // declarer en "string" revient a declarer que la colone sera en varchar 255.
      */
     public $title;
+
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    public $image;
+
+
+    /**
+     * @ORM\Column(type="boolean") //pour vérifier si vrai
+     */
+    public $isPublished;
+
+
+    /**
+     * @ORM\Column (type="string")
+     */
+    public $autor;
 }
+
+//- mappez ces propriétés avec les annotations @ORM
+//- générez la requête SQL (migration) avec php bin/console make:migration
+//- exécutez la migration en bdd pour créer la table avec php bin/console doctrine:migration:migrate
+
+//Création ou modif d'une entité (classe avec les annotaiton @ORM)
+//- php bin/console make:migration pour générer la requête SQL (pour créer / modifier la /les tables)
+//- php bin/console doctrine:migration:migrate pour envoyer la requête SQL en bdd
