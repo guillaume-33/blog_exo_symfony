1a367fc47ce1287dbc9f4f9b3e0393e3bc8dda05
guillaume <laloum.guillaume@gmail.com>
2022-07-06 10:39:36 +0200

    exo BDD +comm

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -5,20 +5,20 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity()
+ * @ORM\Entity() // permet de déclarer que les classes seront des entités
  */
-class Article
+class Article // le nom de notre table(entité) TOUJOURS AVEC lA PREMIERE LETTRE EN MAJ !!!!
 {
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
+    /** on declare toujours les 'attributs' avant l'entrée.
+     * @ORM\Id() // on commence par déclarer l'id de notre table
+     * @ORM\GeneratedValue()// on definie que l'id sera en auto incremente
+     * @ORM\Column(type="integer") //et qu'elle sera de type integer
      */
     public $id;
 
 
     /**
-     * @ORM\Column(type="string")
+     * @ORM\Column(type="string") // declarer en "string" revient a declarer que la colone sera en varchar 255.
      */
     public $title;
 }
