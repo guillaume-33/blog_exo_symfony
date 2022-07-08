3f679833069a915e0c82b26830d066ca55bd603c
guillaume <laloum.guillaume@gmail.com>
2022-07-08 11:55:13 +0200

    page admin categories

--- src/Controller/AdminArticlesController.php
+++ src/Controller/AdminArticlesController.php
@@ -124,6 +124,22 @@ class AdminArticlesController extends AbstractController
            return $this->redirectToRoute('home'); //renvoie a la page accueil
         }
 
+    /**
+     * @Route("/admin/article/{id}", name="admin_article")
+     */
+//------ nouvelle route sans will card grace aux données récupérées via ArticleRepository.
+//methode qui va "remplacer" le SELECT FROM WHERE
+// on appel une instance de la classe ArticleRepository
+    public function articleSolo(ArticleRepository $articleRepository, $id)
+    {
+        $article= $articleRepository ->find($id);
+        //"find" permet de récuperer l'id de la table selectionnée
+
+        //puis on renvoie sur la page twig
+        return $this->render('admin/article.html.twig',
+            ['article'=> $article]);
+    }
+
 
 
 }
