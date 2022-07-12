8b89a97af98c20bcab171dba24e8983005c2f528
guillaume <laloum.guillaume@gmail.com>
2022-07-12 13:57:31 +0200

    ajout de champ form dans l'entite article

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -16,12 +16,12 @@ class ArticleType extends AbstractType
     {
         $builder
             ->add('tilte')
-            //--------------------------------------------------
+
             ->add('category',EntityType::class,[
                     'class'=>Category::class,
-                    'choice_label'=>'title'
+                    'choice_label'=>'title',
                 ])
-           // permet d'ajouter une " option" dans la création de lentité article, grace à sa cardinalité avec l'entité "category". on commence par choisir l'entité que l'on veut" raccorder" puis on choisis ce que l'on veut rajouter ( ici, le titre de la category)
+           // permet d'ajouter une " option" dans la création de l'entité article, grace à sa cardinalité avec l'entité "category". on commence par choisir l'entité que l'on veut" raccorder" puis on choisis ce que l'on veut rajouter ( ici, le titre de la category)
             ->add('image')
             ->add('isPublished')
             ->add('author')
